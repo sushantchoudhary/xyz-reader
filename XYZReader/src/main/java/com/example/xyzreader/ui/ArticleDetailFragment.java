68dc3d2ef02c96b9a411d1c3f37051bacc4145a9
Sushant Kumar <schoudhary@atlassian.com>
2019-03-06 23:25:23 +1100

    Updating typeface and size attribs

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -207,7 +207,7 @@ public class ArticleDetailFragment extends Fragment implements
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
 
 
-        bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
+        bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "OpenSans-Regular.ttf"));
 
         if (mCursor != null) {
             mRootView.setAlpha(0);
